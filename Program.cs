bfc6365b5bcdb72590320e2a8139d7e2bad45b0e
Gastón <benitez30.gnb@gmail.com>
2020-02-05 19:02:47 -0300

    no message

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double v = 0.1429;
+            DateTime fecha1 = new DateTime(2020,1,31);
+            DateTime fecha2 = new DateTime(2015,1,31);
+            Console.WriteLine(fecha1.ToLongDateString());
+            Console.WriteLine(fecha2.ToLongDateString());
+            TimeSpan calculo = fecha1 - fecha2;
+            int dias = calculo.Days;
+            Console.WriteLine(" {0} Es el número de dias de una fecha a la otra", dias);
+            //Console.WriteLine("{0} Son las semanas",                                          ver forma simple de convertir dias en semanas.
+            Console.Read();
+
+            
+        }
+
+    }
+}
